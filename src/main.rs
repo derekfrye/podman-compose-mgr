71ab837f2ef226e33e88b6fd417016e8e45c4c97
Derek Frye <derek.frye@gmail.com>
2024-06-27 01:18:49 -0500

    image_refresh: takes a param and other fixes

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
-use walkdir::WalkDir;
+use clap::Parser;
 use regex::Regex;
 use std::fs::File;
-use std::io::{self, BufRead};
+use std::io::{self, BufRead, Write};
+use walkdir::WalkDir;
 // use std::path::Path;
 use std::process::Command;
 
+/// Struct to handle command-line arguments
+#[derive(Parser)]
+struct Args {
+    /// Path to the directory to traverse
+    #[clap(long)]
+    path: String,
+}
+
 fn main() -> io::Result<()> {
+    // Parse command-line arguments
+    let args = Args::parse();
+
     // Define the pattern to match image names
     let pattern = Regex::new(r"^\s*image:\s*(.*)").unwrap();
-    let djf_pattern = Regex::new(r"^djf/[\w]+$").unwrap();
+    let djf_pattern = Regex::new(r"^\s*djf/[\w\d:._-]+$").unwrap();
+    // let djf_pattern = Regex::new(r"^djf/[\w]+(:[\w][\w.-]{0,127})?$").unwrap();
 
-    // Traverse the current directory and subdirectories
-    for entry in WalkDir::new(".").into_iter().filter_map(|e| e.ok()) {
+    // Traverse the specified directory and subdirectories
+    for entry in WalkDir::new(&args.path).into_iter().filter_map(|e| e.ok()) {
         // Check if the file is docker-compose.yml
         if entry.file_type().is_file() && entry.file_name() == "docker-compose.yml" {
             // Open the file and read it line by line
@@ -23,13 +36,37 @@ fn main() -> io::Result<()> {
                             let image = captures.get(1).unwrap().as_str().trim();
                             // Check if the image matches the djf pattern
                             if !djf_pattern.is_match(image) {
-                                // Pull the image using podman
-                                println!("Pulling image: {}", image);
-                                let _output = Command::new("podman")
-                                    .arg("pull")
-                                    .arg(image)
-                                    .output()
-                                    .expect("Failed to execute podman pull");
+                                // Ask user if they want to refresh the image
+                                let z = entry
+                                    .path()
+                                    .parent()
+                                    .unwrap_or(std::path::Path::new("/"))
+                                    .display();
+                                let mut z_display = format!("{}", z);
+                                let z_len = z_display.len();
+                                if z_len > 25 {
+                                    let start = &z_display[..5];
+                                    let end = &z_display[z_len - 20..];
+                                    z_display = format!("{}...{}", start, end);
+                                    // println!("{}", result);
+                                }
+                                print!("Refresh '{}' from {}? (y/N): ", image, z_display);
+                                let mut input = String::new();
+                                io::stdout().flush().unwrap();
+                                io::stdin().read_line(&mut input).unwrap();
+                                let input = input.trim();
+                                if input.eq_ignore_ascii_case("y") {
+                                    // Pull the image using podman
+                                    println!("Pulling image: {}", image);
+                                    let output = Command::new("podman")
+                                        .arg("pull")
+                                        .arg(image)
+                                        .output()
+                                        .expect("Failed to execute podman pull");
+                                    io::stdout().write_all(&output.stdout).unwrap();
+                                } else {
+                                    println!("Skipping image: {}", image);
+                                }
                             }
                         }
                     }
