844271657335baf3dacf65290ffd6a81ebb29cd7
Derek Frye <derek.frye@gmail.com>
2024-07-11 15:21:05 -0500

    start to build secrets refresh

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,44 @@
-use clap::Parser;
-use walkdir::WalkDir;
+// use clap::builder::Str;
+use std::path::Path;
+use clap::{Parser, ValueEnum};
 use regex::Regex;
 use std::fs::File;
-use std::io::{self, BufRead, Write, BufReader};
+use std::io::{self, BufRead, BufReader, Write};
 use std::process::{Command, Stdio};
+use walkdir::WalkDir;
 
 /// Struct to handle command-line arguments
 #[derive(Parser)]
 struct Args {
     /// Path to the directory to traverse
-    #[clap(long)]
+    #[arg(short = 'p', long, value_name = "PATH", default_value = ".")]
     path: String,
+    /// Mode to run the program in
+    #[arg(short = 'm', long, default_value = "Rebuild", value_parser = clap::value_parser!(Mode))]
+    mode: Mode,
 }
+
+/// Enumeration of possible modes
+#[derive(Clone, ValueEnum)]
+enum Mode {
+    Rebuild,
+    Secrets,
+}
+
 
 fn main() -> io::Result<()> {
     // Parse command-line arguments
     let args = Args::parse();
 
+    match args.mode {
+        Mode::Rebuild => rebuild(&args),
+        Mode::Secrets => secrets(&args),
+    }
+
+    Ok(())
+}
+
+fn rebuild(args: &Args){
     // Define the pattern to match image names
     let pattern = Regex::new(r"^\s*image:\s*(.*)").unwrap();
     // Update the pattern to match the required image format with tags
@@ -78,6 +100,65 @@ fn main() -> io::Result<()> {
             }
         }
     }
-
-    Ok(())
 }
+
+
+fn secrets(args: &Args) {
+    // Define the pattern to match secrets
+    let pattern = Regex::new(r"^\s*-\s*([\w\d_]+)").unwrap();
+
+    for entry in WalkDir::new(&args.path).into_iter().filter_map(|e| e.ok()) {
+        if entry.file_type().is_file() && entry.file_name() == "docker-compose.yml" {
+            if let Ok(file) = File::open(entry.path()) {
+                for line in io::BufReader::new(file).lines() {
+                    if let Ok(line) = line {
+                        if let Some(captures) = pattern.captures(&line) {
+                            let secret = captures.get(1).unwrap().as_str().trim();
+                            // Check if the secret exists
+                            let secret_path = Path::new(&args.path).join("secrets").join(secret);
+                            if !secret_path.exists() {
+                                let z = entry.path().parent().unwrap_or(Path::new("/")).display();
+                                let mut z_display = format!("{}", z);
+                                let z_len = z_display.len();
+                                if z_len > 25 {
+                                    let start = &z_display[..5];
+                                    let end = &z_display[z_len - 20..];
+                                    z_display = format!("{}...{}", start, end);
+                                }
+                                print!("Create secret '{}' for {}? (y/N): ", secret, z_display);
+                                io::stdout().flush().unwrap();
+                                let mut input = String::new();
+                                io::stdin().read_line(&mut input).unwrap();
+                                let input = input.trim();
+                                if input.eq_ignore_ascii_case("y") {
+                                    // Create the secret using podman and stream the output
+                                    println!("Creating secret: {}", secret);
+                                    let mut child = Command::new("podman")
+                                        .arg("secrets")
+                                        .arg("create")
+                                        .arg(secret)
+                                        .arg(secret_path)
+                                        .stdout(Stdio::piped())
+                                        .spawn()
+                                        .expect("Failed to execute podman secrets create");
+
+                                    if let Some(stdout) = child.stdout.take() {
+                                        let reader = BufReader::new(stdout);
+                                        for line in reader.lines() {
+                                            if let Ok(line) = line {
+                                                println!("{}", line);
+                                            }
+                                        }
+                                    }
+
+                                    let _ = child.wait().expect("Command wasn't running");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
