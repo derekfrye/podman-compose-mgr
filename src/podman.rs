588f61b434a8be6dffe75f5ccb1f37bd15db770b
Derek Frye <derek.frye@gmail.com>
2024-08-16 20:07:29 -0500

    image_rebuild: "n" works, "d" works

--- src/podman.rs
+++ src/podman.rs
@@ -1,10 +1,11 @@
 use std::process::Command;
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, TimeZone, Utc};
 
 
 
 pub fn get_podman_image_refresh_time(img: &str ) -> Result<DateTime<Utc>, String> {
     let mut cmd = Command::new("podman");
+    cmd.arg("image");
     cmd.arg("inspect");
     cmd.arg("--format");
     cmd.arg("{{.Created}}");
@@ -15,6 +16,11 @@ pub fn get_podman_image_refresh_time(img: &str ) -> Result<DateTime<Utc>, String
         let x = convert_str_to_date(stdout.trim());
         Ok(x?)
     } else {
+        // if error = image not known, then just return 1/1/1900
+        if std::str::from_utf8(&output.stderr).unwrap().contains("image not known") {
+            let dt = Utc.with_ymd_and_hms(1900, 1, 1, 0, 0, 0).unwrap();
+            return Ok(dt);
+        }
         let stderr = String::from_utf8(output.stderr).map_err(|e| format!("Failed to parse podman output: {}", e))?;
         Err(format!("podman failed: {}", stderr))
     }
