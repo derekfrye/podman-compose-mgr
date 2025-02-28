1ff14daff63a6732b1ba176de9acadf3d4183a0f
Derek Frye <derek.frye@gmail.com>
2025-02-27 21:22:10 -0600

    preparing for tests

--- tests/test1.rs
+++ tests/test1.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{self, Write};
+use std::io::{self};
 use tempfile::NamedTempFile;
 
 #[test]
@@ -7,12 +7,12 @@ fn test1() -> io::Result<()> {
     let temp_file = NamedTempFile::new_in("/tmp")?;
     let file_path = temp_file.path().to_owned();
 
-    writeln!(temp_file.as_file(), "This is a temporary file!")?;
-
-    println!("Created unique file: {}", file_path.display());
-
+    // writeln!(temp_file.as_file(), "This is a temporary file!")?;
+    // println!("Created unique file: {}", file_path.display());
     // If you want to persist the file instead of it being deleted when `temp_file` is dropped:
     let _persisted_file = File::create(&file_path)?;
+
+    
 
     //now remove it
     temp_file.close()?;
